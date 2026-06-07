c024cb491843a2908a717a1baa5830fb4fbf3ecd
AYUSH YADAV <ayushkumar85890@gmail.com>
2026-06-08 00:41:42 +0530

    union intersection

--- a1.py
+++ a1.py
@@ -1,39 +1,4 @@
-student={
-    "ayush yadav":{
-        "name":"ayush yadav",
-        "roll":157,
-        "marks":400,
-        "course":"bca",
-        "collage":"iibm",
-        "semester":"first",
-        "year":"first"
-        
-
-    },
-         "bobby kumar":{
-         "name":"bobby kumar",
-        "roll":148,
-        "marks":400,
-        "course":"bca",
-        "collage":"iibm",
-        "semester":"first",
-        "year":"first"
-        
-
-    }
-  
-}
-
-name=input("ENTER STUENT NAME:")
-
-if name in student:
-    print("name",student[name]["name"])
-    print("roll",student[name]["roll"])
-    print("marks",student[name]["marks"])
-    print("course",student[name]["course"])
-    print("collage",student[name]["collage"])
-    print("semester",student[name]["semester"])
-    print("year",student[name]["year"])
-
-else:
-    print("student not found")
+s1={11,1,11,44,22,44,11,22,1}
+s2={45,44,12,11,55,1,2}
+print(s1.intersection(s2))
+print(s1.union(s2))
